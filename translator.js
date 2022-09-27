f5d801b4c22480d755eec46c0439520dd263f923
PYUDNG <63054558+PYUDNG@users.noreply.github.com>
2022-09-28 01:06:59 +0800

    Add files via upload

--- translator.js
+++ translator.js
@@ -175,7 +175,7 @@
 						method: 'GET',
 						headers: {
 							'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
-						}
+						},
 						url: url,
 						onload: function(response) {
 							callback(response.responseText);
