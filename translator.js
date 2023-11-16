959c37ff0754cbf21dba30004e21da37554cb9aa
PYUDNG <PY_DNG@163.com>
2023-11-17 00:53:02 +0800

    sign calculator update

--- translator.js
+++ translator.js
@@ -57,6 +57,10 @@
 				}
 				para = para.substr(1);
 
+				// Trim before translate and record trimmed string
+				const trim = trimText(para);
+				para = trim.trimmed;
+
 				// Whether this paragraph contains \n at beginning
 				const countNewLine = str => str.split('').filter(t => t === '\n').length;
 				const newline_begin = countNewLine(para.match(/^\s*/) ? para.match(/^\s*/)[0] : '');
@@ -76,7 +80,7 @@
 						const temp_result = json.trans_result.data.reduce(function(pre, cur) {
 							return (pre.push('\n'.repeat(cur.prefixWrap) + cur.dst), pre);
 						}, []).join('\n');
-						result[i].dst = temp_result;
+						result[i].dst = trim.trimLeft + temp_result + trim.trimRight;
 						AM.finish();
 
 						onparamload(AM.taskCount);
@@ -187,36 +191,63 @@
 
 			// Calc sign based on query-text and gtk
 			function calcSign(query) {
-				function a(r, o) {
-				    for (var t = 0; t < o.length - 2; t += 3) {
-				        var a = o.charAt(t + 2);
-				        a = a >= "a" ? a.charCodeAt(0) - 87 : Number(a),
-				        a = "+" === o.charAt(t + 1) ? r >>> a: r << a,
-				        r = "+" === o.charAt(t) ? r + a & 4294967295 : r ^ a
-				    }
-				    return r
+				function e(t, e) {
+					(null == e || e > t.length) && (e = t.length);
+					for (var n = 0, r = new Array(e); n < e; n++)
+						r[n] = t[n];
+					return r
 				}
-				var C = null;
-				var token = function(r, _gtk) {
-				    var o = r.length;
-				    o > 30 && (r = "" + r.substr(0, 10) + r.substr(Math.floor(o / 2) - 5, 10) + r.substring(r.length, r.length - 10));
-				    var t = void 0,
-				    t = null !== C ? C: (C = _gtk || "") || "";
-				    for (var e = t.split("."), h = Number(e[0]) || 0, i = Number(e[1]) || 0, d = [], f = 0, g = 0; g < r.length; g++) {
-				        var m = r.charCodeAt(g);
-				        128 > m ? d[f++] = m: (2048 > m ? d[f++] = m >> 6 | 192 : (55296 === (64512 & m) && g + 1 < r.length && 56320 === (64512 & r.charCodeAt(g + 1)) ? (m = 65536 + ((1023 & m) << 10) + (1023 & r.charCodeAt(++g)), d[f++] = m >> 18 | 240, d[f++] = m >> 12 & 63 | 128) : d[f++] = m >> 12 | 224, d[f++] = m >> 6 & 63 | 128), d[f++] = 63 & m | 128)
-				    }
-				    for (var S = h,
-				    u = "+-a^+6",
-				    l = "+-3^+b+-f",
-				    s = 0; s < d.length; s++) S += d[s],
-				    S = a(S, u);
-
-				    return S = a(S, l),
-				    S ^= i,
-				    0 > S && (S = (2147483647 & S) + 2147483648),
-				    S %= 1e6,
-				    S.toString() + "." + (S ^ h)
+				function n(t, e) {
+					for (var n = 0; n < e.length - 2; n += 3) {
+						var r = e.charAt(n + 2);
+						r = "a" <= r ? r.charCodeAt(0) - 87 : Number(r),
+						r = "+" === e.charAt(n + 1) ? t >>> r : t << r,
+						t = "+" === e.charAt(n) ? t + r & 4294967295 : t ^ r
+					}
+					return t
+				}
+				var r = null;
+				var token = function(t, _gtk) {
+					var o, i = t.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/g);
+					if (null === i) {
+						var a = t.length;
+						a > 30 && (t = "".concat(t.substr(0, 10)).concat(t.substr(Math.floor(a / 2) - 5, 10)).concat(t.substr(-10, 10)))
+					} else {
+						for (var s = t.split(/[\uD800-\uDBFF][\uDC00-\uDFFF]/), c = 0, u = s.length, l = []; c < u; c++)
+							"" !== s[c] && l.push.apply(l, function(t) {
+								if (Array.isArray(t))
+									return e(t)
+							}(o = s[c].split("")) || function(t) {
+								if ("undefined" != typeof Symbol && null != t[Symbol.iterator] || null != t["@@iterator"])
+									return Array.from(t)
+							}(o) || function(t, n) {
+								if (t) {
+									if ("string" == typeof t)
+										return e(t, n);
+									var r = Object.prototype.toString.call(t).slice(8, -1);
+									return "Object" === r && t.constructor && (r = t.constructor.name),
+									"Map" === r || "Set" === r ? Array.from(t) : "Arguments" === r || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r) ? e(t, n) : void 0
+								}
+							}(o) || function() {
+								throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")
+							}()),
+							c !== u - 1 && l.push(i[c]);
+						var p = l.length;
+						p > 30 && (t = l.slice(0, 10).join("") + l.slice(Math.floor(p / 2) - 5, Math.floor(p / 2) + 5).join("") + l.slice(-10).join(""))
+					}
+					for (var h = (_gtk || "").split("."), f = Number(h[0]) || 0, m = Number(h[1]) || 0, g = [], y = 0, v = 0; v < t.length; v++) {
+						var _ = t.charCodeAt(v);
+						_ < 128 ? g[y++] = _ : (_ < 2048 ? g[y++] = _ >> 6 | 192 : (55296 == (64512 & _) && v + 1 < t.length && 56320 == (64512 & t.charCodeAt(v + 1)) ? (_ = 65536 + ((1023 & _) << 10) + (1023 & t.charCodeAt(++v)),
+						g[y++] = _ >> 18 | 240,
+						g[y++] = _ >> 12 & 63 | 128) : g[y++] = _ >> 12 | 224,
+						g[y++] = _ >> 6 & 63 | 128),
+						g[y++] = 63 & _ | 128)
+					}
+					for (var b = f, w = '+-a^+6', k = '+-3^+b+-f', x = 0; x < g.length; x++)
+						b = n(b += g[x], w);
+					return b = n(b, k),
+					(b ^= m) < 0 && (b = 2147483648 + (2147483647 & b)),
+					"".concat((b %= 1e6).toString(), ".").concat(b ^ f)
 				}
 
 				return token(query, BT.gtk);
@@ -254,6 +285,17 @@
 					});
 				}
 			}
+		}
+
+		function trimText(text) {
+			const unTrimmed = text;
+			const trimmed = text.trim();
+			const trimStart = unTrimmed.indexOf(trimmed);
+			const trimEnd = trimStart + trimmed.length;
+			const trimLeft = unTrimmed.substring(0, trimStart);
+			const trimRight = unTrimmed.substring(trimEnd);
+
+			return {unTrimmed, trimmed, trimStart, trimEnd, trimLeft, trimRight};
 		}
 
 		function AsyncManager() {
